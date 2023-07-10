edfab5300db3a5065af2b7c4cdece5a9f166b2fb
Akshitha A <sreeakshitha.a91@gmail.com>
2023-07-10 12:52:46 -0400

    Initial Commit

---
+++ NYCSchoolsSwiftUITest/NYCSchoolsSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsSwiftUITestApp.swift
+//  NYCSchoolsSwiftUITest
+//
+//  Created by Akshitha atmakuri on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
